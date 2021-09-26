a4e4ee0b094aba31f3f3423f75b011e794cbe776
Ben Ousborne <bousborne@gmail.com>
2021-09-25 21:33:39 -0600

    PageAllocator fixed

--- interviews/pageAllocator.c
+++ interviews/pageAllocator.c
@@ -2,23 +2,23 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define BITSIZE 64
+#define BITSIZE 32
 
-int testBit(uint64_t* arr, int k){
+int testBit(int* arr, int k){
     return ((arr[k/BITSIZE] & (1 << (k%BITSIZE) )) !=0);
 }
 
-void setBit(uint64_t* arr, int k){
+void setBit(int* arr, int k){
     arr[k/BITSIZE] |= 1 << (k%BITSIZE);
 }
 
-void clearBit(uint64_t* arr, int k){
+void clearBit(int* arr, int k){
     arr[k/BITSIZE] &= ~(1 << (k%BITSIZE));
 }
 
-void printPages(uint64_t* arr, int ROWS, size_t numBits) {
+void printPages(int* arr, int ROWS, size_t numBits) {
     for (int i=0; i<ROWS; i++) {
-        printf("%ld\n", arr[i]);
+        printf("%d\n", arr[i]);
     }
     for (int i=0; i<ROWS; i++) {
 	for (int j=0; j<BITSIZE; j++) {
@@ -28,25 +28,19 @@ void printPages(uint64_t* arr, int ROWS, size_t numBits) {
     }
 }
 
-//void setBit(int** arr[], int k){
-//    arr[k/BITSIZE] |= 1 << (k%BITSIZE);
-//}
-
-//int testBit(uint64_t* arr, int k){
-//    return ((arr[k/BITSIZE] & (1 << (k%BITSIZE) )) !=0);
-//}
-
-int pageAllocator(uint64_t* arr, int ROWS, size_t numBits) {
+int pageAllocator(int* arr, int ROWS, size_t numBits) {
     int location=0;
     int count=0;
     
     for (int i=0; i<ROWS; i++) {
         for (int j=0; j<BITSIZE; j++) {
             if ((arr[i] & (1 << j%BITSIZE)) == 0) {
-                location = (i/BITSIZE) + j;
+                location = (i*BITSIZE) + j;
+		
 		count++;
+		printf("location(%d), count(%d)\n", location, count);
 		if (count == 3) {
-                   location = location - 3;
+                   location = location - 3 + 1;
 		   return location;
 		}
 	    } else {
@@ -57,67 +51,36 @@ int pageAllocator(uint64_t* arr, int ROWS, size_t numBits) {
    
     if (count < 3) location = -1;
 
-    return location;
+    return location + 1;
 }
 
 int main() {
     int numBits = 64;
     int ROWS = 4;
-    //uint64_t arr[4] = {1111111101001110011111111111111111111111010011100111111111111111,
-    //	          1111111101001110011111111111111111111111010011100111111111111111,
-    //		  1111111101001110011111111111111100011111000111111111111111111111,
-    //		  1111111101001110011111111111111111111111010011100111111111111111};
-
-    //uint64_t arr[ROWS];
-
     
-    uint64_t* arr = (uint64_t*)malloc(ROWS * sizeof(uint64_t));
-    //for (int i=0; i<ROWS; i++) {
-    //    arr[i] = (uint64_t*)malloc(sizeof(uint64_t *));
-    //}
+    int* arr = (int*)malloc(ROWS * sizeof(int));
     for (int i=0; i <ROWS; i++) {
         arr[i] = 0;
-        //arr[i] = 1;
     }
-    //for (int i=0; i<ROWS; i++) {
-    //    setBit(arr, j*i);
-    //}
-    //PageAllocator* pageAlloc = new PageAllocator();
-    //pageAlloc.pageAllocator(arr, numBits);
-
-    //for (int i=0; i<sizeof(arr); i++) {
-    //    for (int j=0; j<BITSIZE; j++) {
-    //        setBit(arr, j*i);
-    //	}
-    //}
 
     for (int i=0; i<ROWS; i++) {
         for (int j=0; j<BITSIZE; j++) {
             setBit(arr, (i*BITSIZE)+j);
 	}
     }
-    clearBit(arr, 129);
-    clearBit(arr, 130);
-    clearBit(arr, 131);
+    clearBit(arr, 0);
+    clearBit(arr, 32);
+    clearBit(arr, 24);
+    clearBit(arr, 25);
+    clearBit(arr, 26);
+    clearBit(arr, 31);
+    
+    clearBit(arr, 33);
     printPages(arr, ROWS, numBits);
     int pageBegin = pageAllocator(arr, ROWS, numBits);
 
-    //printf("Array:\n");
-    //for (uint64_t i = 0; i < ROWS; i++)
-    //{
-    //   for (uint64_t j = 0; j < BITSIZE; j++)
-    //    {
-    //        uint64_t val = arr[i];
-    //        if (val < 10)
-    //        {
-    //           printf(" ");
-    //        }
-    //        printf(" %ld", arr[i]);
-    //    }
-    //    printf("\n");
-    //}
 
-    for (uint64_t i = 0; i < ROWS; i++)
+    for (int i = 0; i < ROWS; i++)
     {
         //free(arr[i]);
     }
